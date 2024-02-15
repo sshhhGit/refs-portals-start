3c30d51c768cb08ac935836bb8ac3556232cc656
sshhhgit <seungho0497@hanmail.net>
2024-02-16 08:47:55 +0900

    ~152

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,30 +1,37 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 
-const ResultModal = forwardRef(function ResultModal({result, targetTime }, ref) {
-    const dialog = useRef();
+const ResultModal = forwardRef(function ResultModal(
+  { targetTime, remainingTime },
+  ref
+) {
+  const dialog = useRef();
 
-    useImperativeHandle(ref, () => {
-        return {
-            open() {
-                dialog.current.showModal();
-            }
-        };
-    });
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
-    return (
-        <dialog ref={dialog} className="result-modal">
-        <h2>You {result}</h2>
-        <p>
-            The target time was <strong>{targetTime} seconds.</strong>
-        </p>
-        <p>
-            You stopped the timer with <strong>X seconds left.</strong>
-        </p>
-        <form method="dialog">
-            <button>Close</button>
-        </form>
-        </dialog>
-    );
-}) 
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  return (
+    <dialog ref={dialog} className="result-modal">
+      {userLost && <h2>You {result}</h2>}
+      <p>
+        The target time was <strong>{targetTime} seconds.</strong>
+      </p>
+      <p>
+        You stopped the timer with{" "}
+        <strong>{formattedRemainingTime} seconds left.</strong>
+      </p>
+      <form method="dialog">
+        <button>Close</button>
+      </form>
+    </dialog>
+  );
+});
 
 export default ResultModal;
